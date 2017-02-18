5417cca372fbe3fb2048e753c3cc54245828a9f1
Joel Wilsson <joel.wilsson@gmail.com>
2017-02-18 22:58:10 +0100

    Add details required by Maven central to project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,14 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :aot [clj-bean.bean-test])
+  :aot [clj-bean.bean-test]
+
+  :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                                    :creds :gpg}
+                        "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
+                                     :creds :gpg}}
+  :scm {:url "git@github.com:wjoel/clj-bean.git"}
+  :pom-addition [:developers [:developer
+                              [:name "Joel Wilsson"]
+                              [:url "https://wjoel.com"]
+                              [:email "joel.wilsson@gmail.com"]]])
